cc333b3be14d3a53099f4e3449eacf1de2628004
deggerfire <70328071+deggerfire@users.noreply.github.com>
2024-02-05 13:30:56 -0600

    gui demo version 2

--- app.R
+++ app.R
@@ -1,5 +1,8 @@
-library(shiny)
-library(ggplot2)
+library(shiny)  # The sever thingy
+library(ggplot2)# Used for plotting
+
+# Demo data just for testing TODO: Remove
+ddt <- read.csv(file("DDT.csv"))
 
 # Define UI for the application
 
@@ -10,29 +13,35 @@ library(ggplot2)
   # seperated out two sections of code for Group A and Group L.
 
 ui <- fluidPage(
+  ######################################################################
+  ########################  Universal Boundary #########################
+  ######################################################################
+  
+  # Put in the top bar, just a demo image for now
+  titlePanel(img(src="logo.png")),
+  
+  # Sidebar with demo selector parts
+  sidebarLayout(
+    sidebarPanel(
+      # Get type of graph the user wants
+      selectInput("graph", "Graph Type", list(`Graph Types` = c("ScatterPlot", "Histogram", "Boxplot"))),
+      # Set what the user wants on the Y-axis
+      selectInput("yAxis", "Y axis", list(`options` = c(sort(colnames(ddt))))),
+      # Set what the user wants on the X-axis
+      selectInput("xAxis", "X axis",list(`options` = c(sort(colnames(ddt))))),
+      # Demo for the possible graph tools
+      sliderInput("bins", "Number of bins:", min = 1, max = 50, value = 30)
+    ),
+    
+    # Show a plot of the generated distribution TODO: Make variable
+    mainPanel(
+      plotOutput("scatterPlot")
+    )
+  )
   
   ######################################################################
   ########################  Group A Boundary ###########################
   ######################################################################
-
-    # Application title
-    titlePanel(img(src="logo.png")),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-          div(
-            selectInput("yAxis", "Y axis", list(`options` = c(sort(colnames(ddt))))),
-            " VS ",
-            selectInput("xAxis", "X axis",list(`options` = c(sort(colnames(ddt))))),
-          )
-        ),
-
-        # Show a plot of the generated distribution
-    mainPanel(
-           plotOutput("distPlot")
-        )
-    )
   
   ######################################################################
   ########################  Group L Boundary ###########################
@@ -46,30 +55,25 @@ ui <- fluidPage(
   # Group A and Group L to prevent merge issues.
 
 server <- function(input, output) {
+  ######################################################################
+  ########################  Universal Boundary #########################
+  ######################################################################
   
+  # Display a scatter plot with the data
+  output$scatterPlot <- renderPlot({
+    g = ggplot(ddt, aes(x = ddt[,input$xAxis], y = ddt[,input$yAxis], colour = ddt[,input$yAxis]))
+    g + ggtitle("demo graph")
+    g = g + geom_point()
+    print(g)
+  })
   
   ######################################################################
   ########################  Group A Boundary ###########################
   ######################################################################
 
-    output$distPlot <- renderPlot({
-      g = ggplot(ddt, aes(x = ddt[,input$xAxis], y = ddt[,input$yAxis], colour = ddt[,input$yAxis]))
-      g + ggtitle("demo graph")
-      g = g + geom_point()
-      print(g)
-    })
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    ######################################################################
-    ########################  Group L Boundary ###########################
-    ######################################################################
+  ######################################################################
+  ########################  Group L Boundary ###########################
+  ######################################################################
 }
 
 # This command runs our application- all you have to do to see the ouput is click
