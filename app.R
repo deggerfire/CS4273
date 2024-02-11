d0c33e4a425824884e8dd2147cfe3bf144d29e9e
deggerfire <70328071+deggerfire@users.noreply.github.com>
2024-02-10 20:52:30 -0600

    update for data selection

--- app.R
+++ app.R
@@ -2,10 +2,6 @@ library(shiny)  # The sever thingy
 library(ggplot2)# Used for plotting
 library(dplyr)  # Used for data handling
 
-# Demo data just for testing TODO: Remove
-data <- read.csv(file("CFS-2022.csv"))
-data2 <- read.csv(file("UOF.csv"))
-
 # Define UI for the application
 
 # this is where we need to define different pages/tabs for us, and then 
@@ -20,10 +16,10 @@ ui <- fluidPage(
   ######################################################################
   
   # Put in the top bar, just a demo image for now
-  titlePanel(img(src="logo.png")),
+  titlePanel(img(src = "logo.png")),
   
   # The blue bar on the top and dataset selector
-  fluidRow(style = "background: #4c5cad; color: white", 
+  fluidRow(style = "background: #4c5cad; color: white",
     selectInput("Data_Set", "Data Set", list(`Graph Types` = c("Calls for Service", "Use of force"))),
   ),
   
@@ -72,44 +68,61 @@ server <- function(input, output) {
   ########################  Universal Boundary #########################
   ######################################################################
   # Display a scatter plot with the data
-  output$Barplot <- renderPlot({
-    graph <- ggplot(data, aes(factor(CallSource), fill = CallSource))     # Setup graph data
-    graph = graph + geom_bar(stat = "Count", position = position_dodge()) # Set up the data as a bar chart
-    graph = graph + xlab("Source of Call") + ylab("Amount")               # Set the x/y labels
-    graph = graph + guides(fill=guide_legend(title="Source of Call"))     # Set the title of the legend
-    graph = graph + theme(text = element_text(size = 18))                 # Set the font size
-    print(graph)                                                          # Print the graph
-  })
+  outputBarPlot <- function(){
+    output$Barplot <- renderPlot({
+      graph <- ggplot(data, aes(factor(CallSource), fill = CallSource))     # Setup graph data
+      graph = graph + geom_bar(stat = "Count", position = position_dodge()) # Set up the data as a bar chart
+      graph = graph + xlab("Source of Call") + ylab("Amount")               # Set the x/y labels
+      graph = graph + guides(fill=guide_legend(title="Source of Call"))     # Set the title of the legend
+      graph = graph + theme(text = element_text(size = 18))                 # Set the font size
+      print(graph)                                                          # Print the graph
+    })
+  }
   
-  # Displays a pie chart using data2
-  output$Piechart <- renderPlot({
-    graph <- ggplot(data.frame(table(data2$RACE)), aes(x = "", y = Freq, fill = Var1)) # Set up graph data
-    graph = graph + geom_bar(stat = "identity", width = 1)                             # Set up the data as a bar chart
-    graph = graph + guides(fill=guide_legend(title="Race"))                            # Set the title of the legend
-    graph = graph + theme_void() + theme(text = element_text(size = 18))               # Remove the background and set the font size
-    graph = graph + coord_polar("y", start = 0)                                        # Convert the graph to polar
-    print(graph)                                                                       # Print the graph
-  })
+  # Function to print out a pie chart using ggplot
+  outputPieChart <- function(data, legend){
+    output$Piechart <- renderPlot({
+      graph <- ggplot(data.frame(data), aes(x = "", y = Freq, fill = Var1))  # Set up graph data
+      graph = graph + geom_bar(stat = "identity", width = 1)                 # Set up the data as a bar chart
+      graph = graph + guides(fill=guide_legend(title = legend))              # Set the title of the legend
+      graph = graph + theme_void() + theme(text = element_text(size = 18))   # Remove the background and set the font size
+      graph = graph + coord_polar("y", start = 0)                            # Convert the graph to polar
+      print(graph)                                                           # Print the graph
+    })
+  }
   
   # Data Selector TODO: make work
   observeEvent(input$Data_Set,{
-    switch(
-      input$Data_Set,
-      "Calls for Service" = 
-        data <- read.csv(file("CFS-2022.csv")),
-      "Use of force" = 
-        data <- read.csv(file("UOF.csv")),
-    )
+    # Remove if chain at some point
+    if(input$Data_Set == "Calls for Service")
+    {
+      data <- read.csv(file("CFS-2022.csv"))
+      outputBarPlot()
+    }
+    else if(input$Data_Set == "Use of force")
+    {
+      data <- read.csv(file("UOF.csv"))
+      outputPieChart(data = table(data$RACE), legend = "Race")
+    }
+    else{print("Error, invalid selection")}
     
+    
+    groupAeffect()
+    groupLeffect()
   })
   
   ######################################################################
   ########################  Group A Boundary ###########################
   ######################################################################
-
+  groupAeffect <- function(){
+    # At some point group A's if chain will be here
+  }
   ######################################################################
   ########################  Group L Boundary ###########################
   ######################################################################
+  groupLeffect <- function(){
+    # At some point group L's if chain will be here
+  }
 }
 
 # This command runs our application- all you have to do to see the ouput is click
